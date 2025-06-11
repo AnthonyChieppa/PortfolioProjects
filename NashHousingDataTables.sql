9b17c4930b975bc9338f13ab39d72331b8513601
AnthonyChieppa <100956576+AnthonyChieppa@users.noreply.github.com>
2025-06-11 17:57:56 -0400

    Add files via upload

---
+++ NashHousingDataTables.sql
@@ -0,0 +1,23 @@
+Drop Table If Exists NashvilleHousing
+Create Table NashvilleHousing
+(
+UniqueID	float,
+ParcelID	varchar,
+LandUse	varchar,
+PropertyAddress	varchar,
+SaleDate	date,
+SalePrice	varchar,
+LegalReference	varchar,
+SoldAsVacant	varchar,
+OwnerName	varchar,
+OwnerAddress	varchar,
+Acreage	float,
+TaxDistrict	varchar,
+LandValue	float,
+BuildingValue	float,
+TotalValue	float,
+YearBuilt	float,
+Bedrooms	float,
+FullBath	float,
+HalfBath	float
+);
